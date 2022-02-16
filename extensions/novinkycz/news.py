cf5feaf7c09df155b1cc0a8ff09ba1fb96052073
Karel Bašta <bastakka@email.cz>
2022-02-16 20:20:47 +0100

    Async novinky.cz

--- extensions/novinkycz/news.py
+++ extensions/novinkycz/news.py
@@ -1,16 +1,6 @@
 """Module with News class for news cog"""
-import requests
+
 from dateutil import parser
-from bs4 import BeautifulSoup
-
-
-def _get_preview_image_url(url: str) -> str:
-    """Get preview image url from news url"""
-    respone = requests.get(url)
-    if respone.status_code == 200:
-        soup = BeautifulSoup(respone.content, "html.parser")
-        preview_image_url = soup.find("meta", property="og:image")["content"]
-        return preview_image_url
 
 
 class News:
@@ -24,4 +14,3 @@ class News:
         temp_date = news.find("news:publication_date", ns).text
         parsed_date = parser.parse(temp_date)
         self.publication_date = parsed_date.strftime("%H:%M %d.%m.%Y")
-        self.preview_image_url = _get_preview_image_url(self.loc)
--- extensions/novinkycz/novinkycz.py
+++ extensions/novinkycz/novinkycz.py
@@ -4,9 +4,20 @@ import os
 import discord
 import requests
 import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
 from discord.ext import commands, tasks
 from core.basecog import BaseCog
 from .news import News
+
+
+async def _get_preview_image_url(url: str) -> str:
+    """Get preview image url from news url"""
+    respone = requests.get(url)
+    if respone.status_code == 200:
+        soup = BeautifulSoup(respone.content, "html.parser")
+        preview_image_url = soup.find("meta", property="og:image")["content"]
+        return preview_image_url
+
 
 async def _make_new_embed(news) -> discord.Embed:
     """Creates an embed from news"""
@@ -19,11 +30,10 @@ async def _make_new_embed(news) -> discord.Embed:
         name=f"Novinky.cz",
         icon_url="attachment://logo-novinkycz.png",
     )
-    embed.set_footer(
-        text=f"Datum zprávy: {news.publication_date}"
-    )
-    embed.set_image(url=news.preview_image_url)
+    embed.set_footer(text = f"Datum zprávy: {news.publication_date}")
+    embed.set_image(url = await _get_preview_image_url(news.loc))
     return embed
+
 
 class Novinkycz(BaseCog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -31,28 +41,34 @@ class Novinkycz(BaseCog):
         self.dir_path = os.path.dirname(os.path.realpath(__file__))
         self.news_url = "https://www.novinky.cz/sitemaps/sitemap_news.xml"
         self.ns = {
-            "loc" : "http://www.sitemaps.org/schemas/sitemap/0.9",
-            "news" : "http://www.google.com/schemas/sitemap-news/0.9",
+            "loc": "http://www.sitemaps.org/schemas/sitemap/0.9",
+            "news": "http://www.google.com/schemas/sitemap-news/0.9",
         }
 
         self.news_repost_json_path = self.dir_path + "/news_reposts.json"
         self.news_reposts = self._get_news_reposts()
-        
+
         self.news_channels_json_path = self.dir_path + "/news_channels.json"
         self.news_channels = self._get_news_channels()
-    
+
     def _get_news_reposts(self) -> dict:
         """Get news repost from news repost local json file"""
         try:
-            with open(self.news_repost_json_path, "r", encoding="utf-8") as news_reposts_file:
+            with open(
+                self.news_repost_json_path, "r", encoding="utf-8"
+            ) as news_reposts_file:
                 news_reposts = json.load(news_reposts_file)
         except FileNotFoundError:
-            self.logger.warning("Could not find news_repost.json file, making new one...")
-            with open(self.news_repost_json_path, "w", encoding="utf-8") as news_reposts_file:
+            self.logger.warning(
+                "Could not find news_repost.json file, making new one..."
+            )
+            with open(
+                self.news_repost_json_path, "w", encoding="utf-8"
+            ) as news_reposts_file:
                 news_reposts = {}
                 json.dump(news_reposts, news_reposts_file, indent=4)
         return news_reposts
-    
+
     def _get_news_channels(self) -> list:
         """Get news channels from news channels local json file"""
         try:
@@ -78,11 +94,11 @@ class Novinkycz(BaseCog):
         news = {}
         if respone.status_code == 200:
             root = ET.fromstring(respone.content)
-            for new in root.findall('loc:url', self.ns):
+            for new in root.findall("loc:url", self.ns):
                 parsed_new = News(new, self.ns)
                 news[parsed_new.loc] = parsed_new
         return news
-    
+
     def _save_news_reposts(self) -> None:
         """Save repost to repost local json file"""
         with open(self.news_repost_json_path, "w", encoding="utf-8") as reposts_file:
@@ -90,7 +106,9 @@ class Novinkycz(BaseCog):
 
     def _save_news_channels(self) -> None:
         """Save news repost to local json file"""
-        with open(self.news_channels_json_path, "w", encoding="utf-8") as news_channels_file:
+        with open(
+            self.news_channels_json_path, "w", encoding="utf-8"
+        ) as news_channels_file:
             json.dump(self.news_channels, news_channels_file, indent=4)
 
     def _increment_news(self) -> None:
@@ -98,7 +116,9 @@ class Novinkycz(BaseCog):
         for news_repost in self.news_reposts.copy():
             self.news_reposts[news_repost] += 1
             if self.news_reposts[news_repost] >= 336:
-                self.logger.debug("%s has reached 2 weeks, removing from list", news_repost)
+                self.logger.debug(
+                    "%s has reached 2 weeks, removing from list", news_repost
+                )
                 del self.news_reposts[news_repost]
 
     @commands.Cog.listener()
@@ -148,7 +168,7 @@ class Novinkycz(BaseCog):
         return await ctx.send(
             f"{ctx.channel.mention} is already subscribed to novinky.cz"
         )
-    
+
     @commands.has_permissions(administrator=True)
     @commands.command()
     async def novinky_not_here(self, ctx: commands.Context) -> None:
